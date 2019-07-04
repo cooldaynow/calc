fb8c9fcecd67edaaf5b73da5404c138d9c422181
coolday <cooldaynow@gmail.com>
2019-07-04 09:33:08 +0300

    Simple Calculator

--- src/Calc/index.jsx
+++ src/Calc/index.jsx
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 import styles from './index.module.scss';
-import {viewInput, delay, logicInput} from '../Checks/index.jsx';
+import {
+  viewInput,
+  delay,
+  logicInput,
+  logicVariables,
+  equalLogic,
+} from '../Checks/index.jsx';
+import Buttons from '../Buttons';
+
 class Calc extends Component {
   state = {
-    buttons: [
-      ['AC', '/', '*'],
-      [7, 8, 9, '-'],
-      [4, 5, 6, '+'],
-      [1, 2, 3, '='],
-      [0, '.'],
-    ],
     input: '0',
     renderInput: '0',
     info: '',
@@ -17,31 +18,18 @@ class Calc extends Component {
   handleButtons = ev => {
     let value = ev.target.value;
     let input = this.state.input;
-    let sChar = /[\+\-\/\*]/;
-    let pChar = /\d+\.|\.\d+/;
-    let dig = input.split(sChar);
-    let last = dig[dig.length - 1];
-    let lastChar = input.slice(-1);
-
-    let inputVariables = {
-      sChar,
-      value,
-      input,
-      last,
-      lastChar,
-      pChar,
-    };
-    let [inp, val] = logicInput(inputVariables);
-    let renderInput = viewInput(inputVariables);
+    let viewErr = this.viewErr;
+    let output = logicInput(logicVariables(input, value), viewErr);
+    let renderInput = viewInput(logicVariables(input, value));
 
     this.setState(state => ({
-      input: inp + val,
-      info: inp + val,
+      input: output,
+      info: output,
       renderInput: renderInput,
     }));
   };
-   
-  viewErr = (last, input) => {
+
+  viewErr = input => {
     let err = 'DIGIT LIMIT MET';
     delay(300)
       .then(() => {
@@ -55,33 +43,7 @@ class Calc extends Component {
 
   equal = () => {
     let input = this.state.input;
-    let info = '';
-    let renderInput = '';
-    try {
-      if (/\.|\*|\+|-|\//.test(input.slice(-1))) {
-        input = input.slice(0, -1);
-      }
-      if (Number.isNaN(eval(input))) {
-        info = 'This calculator does not allow to divide by zero: (';
-      } else if (!isFinite(eval(input))) {
-        info = 'To infinity... and beyond :)';
-      } else {
-        let result = eval(input);
-        info = `${input} = ${result}`;
-        renderInput = result;
-      }
-    } catch (err) {
-      info = 'Please input correct values';
-      console.log(err.name);
-    } finally {
-      this.setState(state => {
-        return {
-          input: '0',
-          info: info,
-          renderInput: renderInput,
-        };
-      });
-    }
+    this.setState(equalLogic(input));
   };
   ac = () => {
     this.setState({
@@ -89,39 +51,6 @@ class Calc extends Component {
       renderInput: '0',
       info: '',
     });
-  };
-  renderButtons = () => {
-    let buttons = this.state.buttons;
-    let sChar = /-|\+|\/|\*/;
-
-    return buttons.map(el =>
-      el.map((elem, i) => (
-        <button
-          onClick={
-            elem === '='
-              ? this.equal
-              : elem === 'AC'
-              ? this.ac
-              : this.handleButtons
-          }
-          id={
-            elem === '='
-              ? styles.equal
-              : elem === 0
-              ? styles.zero
-              : styles[elem]
-          }
-          className={
-            sChar.test(elem)
-              ? `${styles.sChar} ${styles.buttons}`
-              : styles.buttons
-          }
-          value={elem}
-          key={i}>
-          {elem}
-        </button>
-      )),
-    );
   };
   render() {
     return (
@@ -131,17 +60,16 @@ class Calc extends Component {
             <div className={styles.head}>
               <span> Simple Calc </span>
             </div>
-            <div className={styles.info}>
-              {this.state.info}
-            </div>
+            <div className={styles.info}>{this.state.info}</div>
             <div className={styles.wrap__input}>
-              <div className={styles.input}>
-                {this.state.renderInput}
-              </div>
+              <div className={styles.input}>{this.state.renderInput}</div>
             </div>
-            <div className={styles.panel}>
-              {this.renderButtons()}
-            </div>
+            <Buttons
+              handleButtons={this.handleButtons}
+              equal={this.equal}
+              ac={this.ac}
+              viewErr={this.viewErr}
+            />
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import Calc from './Calc';
-import Calc from '../src/Calc/index2.jsx'
+import Calc from '../src/Calc/index.jsx'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Calc/>, document.getElementById('root'));
